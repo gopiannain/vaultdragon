a21e83b830593e9306599668c5ef073e5c0891ba
Arjunan Gopinathan <gopiannain@gmail.com>
2017-05-19 01:05:22 +0800

    update DB
    Update Db

--- Server.js
+++ Server.js
@@ -7,6 +7,30 @@ var router      =   express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
+var options = { server: { socketOptions: { keepAlive: 1 } }, user: "a6cc7a7ecde0b285eee0bf6d85da9d4c",
+pass: "a6cc7a7ecde0b285eee0bf6d85da9d4c" };
+
+var connectionString = '32-1a.mongo.evennode.com:27017,32-1b.mongo.evennode.com:27017';
+
+ mongoose.connect(connectionString, options);
+
+//Add those events to get more info about mongoose connection:
+
+// Connected handler
+mongoose.connection.on('connected', function (err) {
+  console.log("Connected to DB using chain: " + connectionString);
+});
+
+// Error handler
+mongoose.connection.on('error', function (err) {
+  console.log(err);
+});
+
+// Reconnect when closed
+mongoose.connection.on('disconnected', function () {
+   self.connectToDatabase();
+});
+
 router.route("/vault")
     .post(function(req,res){
         var db = new DatabaseOp();
--- models/mongo.js
+++ models/mongo.js
@@ -1,5 +1,5 @@
 var mongoose    =   require("mongoose");
-mongoose.connect('mongodb://localhost:27017/vaultDb');
+
 var mongoSchema =   mongoose.Schema;
 var KVSchema  = {
     "key" : String,
