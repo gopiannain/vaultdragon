f4bba82ff023a315206ec96c675add9dfa1c401f
Arjunan Gopinathan <gopiannain@gmail.com>
2017-05-19 15:51:40 +0800

    Server Details
    Setup the new server details and MongoDB

--- Server.js
+++ Server.js
@@ -7,29 +7,6 @@ var router      =   express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
-var options = { server: { socketOptions: { keepAlive: 1 } }, user: "a6cc7a7ecde0b285eee0bf6d85da9d4c",
-pass: "a6cc7a7ecde0b285eee0bf6d85da9d4c" };
-
-var connectionString = '32-1a.mongo.evennode.com:27017,32-1b.mongo.evennode.com:27017';
-
- mongoose.connect(connectionString, options);
-
-//Add those events to get more info about mongoose connection:
-
-// Connected handler
-mongoose.connection.on('connected', function (err) {
-  console.log("Connected to DB using chain: " + connectionString);
-});
-
-// Error handler
-mongoose.connection.on('error', function (err) {
-  console.log(err);
-});
-
-// Reconnect when closed
-mongoose.connection.on('disconnected', function () {
-   self.connectToDatabase();
-});
 
 router.route("/vault")
     .post(function(req,res){
@@ -73,7 +50,7 @@ router.route("/vault")
             }
             else 
             if (err) {
-                response = {"error" : false,"message" : "Database Error"};
+                response = {"error" : false,"message" : "Database Error", "errmsg": err};
                 res.json(response); 
             }   
         });
--- models/mongo.js
+++ models/mongo.js
@@ -1,5 +1,30 @@
 var mongoose    =   require("mongoose");
 
+var options = { server: { socketOptions: { keepAlive: 1 } }, user: "a6cc7a7ecde0b285eee0bf6d85da9d4c",pass: "a6cc7a7ecde0b285eee0bf6d85da9d4c" };
+
+var connectionString = 'mongodb://32-1a.mongo.evennode.com:27017/a6cc7a7ecde0b285eee0bf6d85da9d4c,mongodb://32-1b.mongo.evennode.com:27017/a6cc7a7ecde0b285eee0bf6d85da9d4c?replicaSet=eusbg1';
+
+ mongoose.connect(connectionString, options);
+
+
+//Add those events to get more info about mongoose connection:
+
+// Connected handler
+mongoose.connection.on('connected', function (err) {
+  console.log("Connected to DB using chain: " + connectionString);
+});
+
+// Error handler
+mongoose.connection.on('error', function (err) {
+  console.log(err);
+});
+
+// Reconnect when closed
+mongoose.connection.on('disconnected', function () {
+   self.connectToDatabase();
+});
+
+
 var mongoSchema =   mongoose.Schema;
 var KVSchema  = {
     "key" : String,
